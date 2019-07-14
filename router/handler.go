de199ad36df46fce2958eeebcb0de7e109af2718
CJ Faraon <faraonc@outlook.com>
2019-07-13 22:14:29 -0700

    Push get foods handler

--- router/handler.go
+++ router/handler.go
@@ -64,21 +64,75 @@ func getFoods(w http.ResponseWriter, r *http.Request) {
 	tags := r.FormValue("tags")
 
 	if strings.TrimSpace(tags) == "" {
-
-	}
-
-	command := `
-				INSERT INTO dough_you.foods(
-					uuid, pic_url, exp_date, created_date 
-				) VALUES($1, $2, $3, $4)
+		command := `
+				SELECT uuid, pic_url, exp_date, created_date FROM dough_you.foods
 				`
-	_, err := postgresDB.Exec(command, uuid.New().String(), picURL, time.Unix(int64(t), 0), time.Now().UTC())
+		row, err := postgresDB.Query(command)
+		if err != nil {
+			utility.RespondWithError(w, http.StatusInternalServerError, "unable to get all foods with tag")
+			return
+		}
+		foods := []map[string]string{}
+		for row.Next() {
+			var uuid, picUrl string
+			var expDate, createdDate time.Time
 
-	if err != nil {
-		utility.RespondWithError(w, http.StatusInternalServerError, "unable to get food")
+			err := row.Scan(&uuid, &picUrl, &expDate, &createdDate)
+			if err != nil {
+				utility.RespondWithError(w, http.StatusInternalServerError, "unable to scan food with tag")
+				return
+			}
+			food := map[string]string{
+				"uuid":         uuid,
+				"pic_url":      picUrl,
+				"exp_date":     expDate.String(),
+				"created_date": createdDate.String(),
+			}
+			foods = append(foods, food)
+		}
+		utility.RespondWithJSON(w, http.StatusOK, foods)
 		return
 	}
-	utility.Respond(w, http.StatusOK, "OK")
+
+	rows := []*sql.Rows{}
+	tagsSlice := strings.Split(tags, ",")
+	for _, tag := range tagsSlice {
+		command := `
+				SELECT uuid, pic_url, exp_date, created_date FROM dough_you.foods
+				INNER JOIN dough_you.tags
+				ON dough_you.foods.uuid = dough_you.tags.food_uuid
+				WHERE dough_you.tags.name = $1
+				`
+		row, err := postgresDB.Query(command, tag)
+		if err != nil {
+			utility.RespondWithError(w, http.StatusInternalServerError, "unable to get food with tag")
+			return
+		}
+		rows = append(rows, row)
+	}
+
+	foods := []map[string]string{}
+	for _, row := range rows {
+		for row.Next() {
+			var uuid, picUrl string
+			var expDate, createdDate time.Time
+
+			err := row.Scan(&uuid, &picUrl, &expDate, &createdDate)
+			if err != nil {
+				utility.RespondWithError(w, http.StatusInternalServerError, "unable to scan food with tag")
+				return
+			}
+			food := map[string]string{
+				"uuid":         uuid,
+				"pic_url":      picUrl,
+				"exp_date":     expDate.String(),
+				"created_date": createdDate.String(),
+			}
+			foods = append(foods, food)
+		}
+	}
+
+	utility.RespondWithJSON(w, http.StatusOK, foods)
 }
 
 func shareFood(w http.ResponseWriter, r *http.Request) {
@@ -88,21 +142,46 @@ func shareFood(w http.ResponseWriter, r *http.Request) {
 	}
 	picURL := r.FormValue("pic_url")
 	expDate := r.FormValue("exp_date")
+	tags := r.FormValue("tags")
+
 	t, err := strconv.Atoi(expDate)
 	if err != nil {
 		utility.RespondWithError(w, http.StatusInternalServerError, "wrong expiration date")
 		return
 	}
+	// TODO post to AWS S3
+
+	if strings.TrimSpace(tags) == "" {
+		utility.RespondWithError(w, http.StatusInternalServerError, "empty tags")
+		return
+	}
+
+	foodUUID := uuid.New().String()
 	command := `
 				INSERT INTO dough_you.foods(
 					uuid, pic_url, exp_date, created_date 
 				) VALUES($1, $2, $3, $4)
 				`
-	_, err = postgresDB.Exec(command, uuid.New().String(), picURL, time.Unix(int64(t), 0), time.Now().UTC())
+	_, err = postgresDB.Exec(command, foodUUID, picURL, time.Unix(int64(t), 0), time.Now().UTC())
 	if err != nil {
-		utility.RespondWithError(w, http.StatusInternalServerError, "unable to share food")
+		utility.RespondWithError(w, http.StatusInternalServerError, "unable to insert food")
 		return
 	}
+
+	tagsSlice := strings.Split(tags, ",")
+	for _, tag := range tagsSlice {
+		command := `
+				INSERT INTO dough_you.tags(
+					food_uuid, name
+				) VALUES($1, $2)
+				`
+		_, err = postgresDB.Exec(command, foodUUID, tag)
+		if err != nil {
+			utility.RespondWithError(w, http.StatusInternalServerError, "unable to insert tags")
+			return
+		}
+	}
+
 	utility.Respond(w, http.StatusOK, "OK")
 }
 
